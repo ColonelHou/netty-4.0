0a275a77bf8512e7f5d95dc70a930d00bd7cc851
houningning <houningning@yiche.com>
2019-07-09 21:15:46 +0800

    chat

--- example/src/main/java/io/netty/example/chat/MyChatClientHandler.java
+++ example/src/main/java/io/netty/example/chat/MyChatClientHandler.java
@@ -1,11 +1,11 @@
-package io.netty.example.socket;
+package io.netty.example.chat;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.time.LocalDateTime;
 
-public class MyClientHandler extends SimpleChannelInboundHandler<String> {
+public class MyChatClientHandler extends SimpleChannelInboundHandler<String> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
         System.out.println(ctx.channel().remoteAddress());
